2d50ad019bc19146fd661b75b129f01049540abe
Stick-0 <166371149+Stick-0@users.noreply.github.com>
2024-10-28 16:48:54 +0900

    Add files via upload

---
+++ twolink_gazebo/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'twolink_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    py_modules=[
+        'src.joint_publisher',
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Action example',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'joint_publisher = twolink_gazebo.joint_publisher:main',
+        ],
+    },
+)
